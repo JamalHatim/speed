d5730f4a201dc9300613922d1d780e001492a762
JamalHatim <hatimjamal8@gmail.com>
2026-01-18 23:14:30 +0100

    rm comm

--- server.v
+++ server.v
@@ -2,7 +2,6 @@ module main
 
 import veb
 
-// Our context struct must embed `veb.Context`!
 pub struct Context {
     veb.Context
 }
@@ -11,7 +10,6 @@ pub struct App {
     veb.StaticHandler
 }
 
-// This is how endpoints are defined in veb. This is the index route
 pub fn (app &App) index(mut ctx Context) veb.Result {
     return $veb.html()
 }
@@ -20,6 +18,5 @@ fn main() {
     mut app := &App{
     }
     app.handle_static('static', true)!
-    // Pass the App and context type and start the web server on port 8080
     veb.run[App, Context](mut app, 8080)
 }
